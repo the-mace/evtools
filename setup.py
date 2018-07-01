5ac36c583a4bbfbb05af93d382da3b7554d93732
rob <rob@crickers.com>
2018-07-01 16:32:24 -0400

    Add setup file and pyenv config

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+setup(name='evtools',
+      version='1.0.0',
+      description='',
+      url='https://github.com/the-mace/evtools',
+      py_modules=['evtools'],
+      author='Rob Mason',
+      license='MIT',
+      )
